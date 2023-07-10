69b064e515db507cd882770870faa37d3f7f49dd
RahafZourob <zourobrahaf@gmail.com>
2023-07-10 22:36:18 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import SectionAbout from './pages/SectionAbout'
+import SectionHeader from './pages/SectionHeader';
+import SectionSpecial from './pages/SectionSpecial';
+import SectionMnue from './pages/SectionMnue'
+import Booking from "./pages/Booking";
+import SectionCooking from "./pages/SectionCooking";
+import SectioFeature from "./pages/SectionFeature";
+import SectionReviews from "./pages/SectionReviews";
+import SectionBlogs from "./pages/SectionBlogs";
+import Subscribe from './pages/Subscribe';
+import Footer from './components/Footer';
+import Contact from './pages/Contact';
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App" >
+      <SectionHeader />
+      <SectionAbout />
+      <SectionSpecial />
+      <SectionMnue />
+      <Booking />
+      <SectionCooking />
+      <SectioFeature />
+      <SectionReviews />
+      <SectionBlogs />
+      <Contact />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
