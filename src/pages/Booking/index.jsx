dc7f7aded8b55abe4f538aee02cce4c76f0f657c
RahafZourob <zourobrahaf@gmail.com>
2023-07-15 00:16:44 +0300

    Add classes

--- src/pages/Booking/index.jsx
+++ src/pages/Booking/index.jsx
@@ -6,16 +6,20 @@ import Transparent from "../../components/Transparent";
 import './style.css'
 export default function Booking() {
     return (
-          <Transparent>
-                <div className="content__booking">
-                <Image src="/assets/booking.png" alt="Booking logo" />
-            <div className="booking__detail">
-                <FeatureHeading variant="flex__rcol" heading="FLAT 10% OFFF"
-                description="Come join us for a lunch this weekend and enjoy" />
-                <ReadMore varint="btn__site">Book Table</ReadMore>
-            </div>
-                <Image src="/assets/booking.png" alt="Booking logo" />
+        <Transparent>
+            <div className="content__booking">
+                <div className="tranlate__xplus">
+                    <Image src="/assets/booking.png" alt="Booking logo" />
                 </div>
-            </Transparent>
+                <div className="booking__detail">
+                    <FeatureHeading variant="flex__rcol" heading="FLAT 10% OFFF"
+                        description="Come join us for a lunch this weekend and enjoy" />
+                    <ReadMore varint="btn__site">Book Table</ReadMore>
+                </div>
+                <div className="translate__xmins">
+                    <Image src="/assets/booking.png" alt="Booking logo" />
+                </div>
+            </div>
+        </Transparent>
     )
 }
